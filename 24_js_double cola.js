244dae3bfdb0f896cb72b4359dcd20193135a400
jlsears <jen.lauren.sears@gmail.com>
2016-04-10 22:10:10 -0500

    restrategizing and refactoring

--- 24_js_double cola.js
+++ 24_js_double cola.js
@@ -12,44 +12,45 @@ function whoIsNext(names, r){
   var amtDoubling = 5;
   var rangeTop = amtDoubling;
   var multCounter = 1;
+  var prevMultCounter = 1;
   var newR = r - 1;
   
   for(var i = 0; i <= newR; i++) {
     console.log("amtDoubling here is: " + amtDoubling);
     
+    // when r is within the original array
     if(r < assessing.length) {
     
       return assessing[r-1];
     }
     
-    if(newR < rangeTop) {
+    // when we need to find r outside of the original array
+    if(r < rangeTop) { // have finally found our range to look further in
       
-      var rangeLow = (rangeTop - amtDoubling) + 1;
+      var rangeLow = (rangeTop - (amtDoubling)) + 1;
       console.log("Hitting rangeLow declaration: " + rangeLow);
       console.log("rangeTop at this point: " + rangeTop);
-      
-      // iterating through each name in assessing
-      for(var k = 0; k <= assessing.length-1; k++) {
-      //console.log("at least hitting first assessing loop");
-      
-        // for each name in assessing, push it into scouting the number of times reflected in multCounter
-        for(var l = 0; l < multCounter; l++) {
-          scouting.push(assessing[k]);
-          console.log("pushing name into scouting: " + assessing[k]);
-        }
+
+      var miniTop = rangeLow + (amtDoubling-1);
+      var miniCounter = 1;
+
+      // this is the range of the person we're looking to return
+      if(r < miniTop) {
+
+        return assessing[miniCounter-1];
       }
-      // looking for the name at index position r in this block
-      for(var m = rangeLow; m <=r; m++) {
-        if(m == r) {
-          console.log("the value of foundIt: " + foundIt);
-          return foundIt = scouting[m];
-          }
-        }  
+
+      else {
+
+        miniTop = miniTop + amtDoubling;
+        miniCounter = miniCounter + 1;
+        } 
       }
     
     else {    
       amtDoubling = amtDoubling * 2;
       rangeTop = rangeTop + amtDoubling;
+      prevMultCounter = multCounter;
       multCounter = multCounter * 2;
     }
   }
