471f9d75d243acf7e9177442dcbebebf667ccc2a
jlsears <jen.lauren.sears@gmail.com>
2016-04-17 18:54:31 -0500

    removing console.logs and unused variables

--- 24_js_double cola.js
+++ 24_js_double cola.js
@@ -1,19 +1,13 @@
 function whoIsNext(names, r){
   var assessing = [];
-  var scouting = [];
-  var foundIt;
-  console.log("the value of r here is: " + r);
-  console.log("the names being presented are: " + names);
   
   for(var j = 0; j < names.length; j++) {
     assessing.push(names[j]);
-    scouting.push(names[j]);
   }
   
   var amtDoubling = assessing.length;
   var rangeTop = amtDoubling;
   var multCounter = 1;
-  var prevMultCounter = 1;
   var newR = r - 1;
   
   for(var i = 0; i <= newR; i++) {
@@ -29,9 +23,6 @@ function whoIsNext(names, r){
     if(r < rangeTop) { // have finally found our range to look further in
       
       var rangeLow = (rangeTop - (amtDoubling)) + 1;
-      console.log("Hitting rangeLow declaration: " + rangeLow);
-      console.log("rangeTop at this point: " + rangeTop);
-
       var miniTop = rangeLow + (multCounter-1);
       var miniCounter = 1;
 
@@ -39,9 +30,7 @@ function whoIsNext(names, r){
 
         // this is the range of the person we're looking to return
         if(r < miniTop) {
-
-          console.log("the winning miniTop: " + miniTop + " and miniCounter: " + miniCounter);
-          console.log("winning multCounter: " + multCounter);
+          
           return assessing[miniCounter-1];
         }
 
@@ -49,8 +38,6 @@ function whoIsNext(names, r){
 
           miniTop = miniTop + multCounter;
           miniCounter = miniCounter + 1;
-          console.log("miniTop is: " + miniTop + " And miniCounter is: " + miniCounter);
-          console.log("multCounter here: " + multCounter);
           }
         }
       }
@@ -58,7 +45,6 @@ function whoIsNext(names, r){
     else {    
       amtDoubling = amtDoubling * 2;
       rangeTop = rangeTop + amtDoubling;
-      prevMultCounter = multCounter;
       multCounter = multCounter * 2;
     }
   }
