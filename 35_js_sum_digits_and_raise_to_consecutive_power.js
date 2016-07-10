f7a8ba8bd7ddd13c99e493627a430400e8415952
jlsears <jen.lauren.sears@gmail.com>
2016-07-10 18:58:33 -0500

    sum digits of number raised to consecutive power

---
+++ 35_js_sum_digits_and_raise_to_consecutive_power.js
@@ -0,0 +1,41 @@
+function sumDigPow(a, b) {
+
+  var answerArray = [];
+  var aValue = a;
+    
+  if(b.toString().length > 1) {
+
+    for(var i = aValue; i < b; i++) {
+      
+      var particular = i.toString().split("");
+      var currentSum = 0;
+      var counter = 1;
+        
+      for(var j = 0; j < particular.length; j++) {
+        
+        currentSum = currentSum + Math.pow(particular[j], counter);
+        counter++; 
+      }
+      if(currentSum == i) { answerArray.push(currentSum); }
+    }
+  }
+  else {
+  for(var i = a; i < b-a; i++) {
+    
+    answerArray.push(i);
+    }  
+  }
+  return answerArray;
+}
+
+// TESTS
+
+Test.describe("Example Tests", function() {
+  Test.assertSimilar(sumDigPow(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9])
+  Test.assertSimilar(sumDigPow(1, 100), [1, 2, 3, 4, 5, 6, 7, 8, 9, 89])
+  Test.assertSimilar(sumDigPow(10, 100),  [89])
+  Test.assertSimilar(sumDigPow(90, 100), [])
+  Test.assertSimilar(sumDigPow(90, 150), [135])
+  Test.assertSimilar(sumDigPow(50, 150), [89, 135])
+  Test.assertSimilar(sumDigPow(10, 150), [89, 135])
+});
